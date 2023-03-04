e6b3759a7d9542740f2a960ca01eba3cf5abcf44
Hest0 <hesto365@gmail.com>
2023-03-04 09:56:52 +0200

    -

--- wardrobe.sql
+++ wardrobe.sql
@@ -23,14 +23,3 @@ VALUES('jeans', 'light_blue', 'Levi\'s', 'slim', 2022),
    ('chinos', 'navy_blue', 'Polo_ralph_lauren', 'slim', 2016),
    ('linen_pants', 'sand', 'Luca_faloni', 'slim_roomy', 2022),
    ('cargo_pants', 'army_green', 'H&M', 'slim', 2021);
-
-CREATE TABLE shorts
- (
-   cloth_id INT AUTO_INCREMENT PRIMARY KEY,
-   item VARCHAR(30) NOT NULL,
-   color VARCHAR(30) NOT NULL,
-   pattern VARCHAR(30),
-   brand VARCHAR(30),
-   other VARCHAR(30),
-   year INT
- );
