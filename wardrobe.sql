cd6488841582ea4b17dad1a490624b59f7640199
Hest0 <hesto365@gmail.com>
2023-03-04 09:59:31 +0200

    Created table 'shorts'

--- wardrobe.sql
+++ wardrobe.sql
@@ -23,3 +23,22 @@ VALUES('jeans', 'light_blue', 'Levi\'s', 'slim', 2022),
    ('chinos', 'navy_blue', 'Polo_ralph_lauren', 'slim', 2016),
    ('linen_pants', 'sand', 'Luca_faloni', 'slim_roomy', 2022),
    ('cargo_pants', 'army_green', 'H&M', 'slim', 2021);
+
+   CREATE TABLE shorts
+ (
+   cloth_id INT AUTO_INCREMENT PRIMARY KEY,
+   item VARCHAR(30) NOT NULL,
+   color VARCHAR(30) NOT NULL,
+   pattern VARCHAR(30),
+   brand VARCHAR(30),
+   other VARCHAR(30),
+   year INT
+ );
+
+INSERT INTO shorts(item, color, pattern, brand, year)
+VALUES('chino_shorts', 'khaki', NULL, 'Super_dry', 2015),
+   ('chino_shorts', 'navy_blue', NULL, 'Polo_ralph_lauren', 2016),
+   ('chino_shorts', 'light_blue', NULL, 'Bershka', 2019),
+   ('chino_shorts', 'dark_blue', NULL, 'Carhartt', 2014),
+   ('swim_shorts', 'navy_blue', 'fish', 'Forever_21', 2018),
+   ('swim_shorts', 'light_green', 'floral', 'Dressmann', 2022);
